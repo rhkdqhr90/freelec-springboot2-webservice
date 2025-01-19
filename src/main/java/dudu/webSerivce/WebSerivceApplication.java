0605b81f65050dba8153638c97a0e97a3eff8d69
Gwangkyo <rhkdqhr08@gmail.com>
2025-01-19 19:48:38 +0900

    Initial commit

---
+++ src/main/java/dudu/webSerivce/WebSerivceApplication.java
@@ -0,0 +1,13 @@
+package dudu.webSerivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSerivceApplication.class, args);
+	}
+
+}

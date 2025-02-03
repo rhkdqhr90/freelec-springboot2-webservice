73dcb5749ddd46a65df9d82d0e45de06e2da836c
Gwangkyo <rhkdqhr08@gmail.com>
2025-02-03 23:19:19 +0900

    Java Toolchain 설정

---
+++ src/test/java/dudu/webservice/web/LongResponse.java
@@ -0,0 +1,14 @@
+package dudu.webservice.web;
+
+public class LongResponse {
+    private Long value;
+
+    // Constructor, Getter, Setter
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+}
